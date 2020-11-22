b26ff6bff17b875f54d133a0c249ffd106992c82
Kaushal Mehta <kaushalkumar304@gmail.com>
2020-11-22 17:38:44 +0530

    updating backend uri

--- src/services/Auth/uri.ts
+++ src/services/Auth/uri.ts
@@ -1,2 +1,3 @@
-export const BASE_URI = "https://cubbard.herokuapp.com/api";
+// export const BASE_URI = "https://cubbard.herokuapp.com/api";
+export const BASE_URI = "http://thecubbard.com/api";
 // export const BASE_URI = "http://192.168.43.164:8000/api";
